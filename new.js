917f18c00100495330f8fa3edfbdeae8e93447a8
ivarcodes <114813138+ivarcodes@users.noreply.github.com>
2024-04-11 21:52:10 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,153 @@
+//console => to print things in console
+// let => to create variable
+// const => to create constant value 
+// Arithmetic operators +,-,%,/,**(pow)
+
+
+
+
+//task 1 to create variables and store values in it :)
+
+// let nam='ravi';
+// let whatyouwannabecomeInYourLife='programmer';
+// let gender='male';
+// let twitterHandle='ivar_writes';
+// console.log(nam,whatyouwannabecomeInYourLife,gender,twitterHandle);
+
+//task 2 operators in js :)
+
+// let firstnum=5;
+// let secondnum=10;
+// console.log(firstnum+secondnum);
+// console.log(firstnum-secondnum);
+// console.log(firstnum * secondnum);
+// console.log(firstnum / secondnum);
+// console.log(firstnum % secondnum);
+// console.log(firstnum**secondnum);
+
+
+//boolean value also defined using let keyword in js :)
+// let vote =false;
+// let isJsprogrammingLanguage=true;
+// let isjshard=false;
+// let favnumb=5;
+// console.log(favnumb+undefined); //Not an number
+
+// console.log(isJsprogrammingLanguage);
+// console.log(isjshard);
+
+// let age =20;
+
+// if(age >= 18){
+//     console.log("You can vote , Happy voting bruda");
+
+// }
+// else{
+// console.log("You cannot Vote" );
+
+// }
+
+// //reverse of string
+
+
+// let nam='ravi';
+// let newname='';
+// for(let i=0;i<nam.length;i++){
+//     newname = nam[i]+newname;
+// }
+// console.log(newname);
+
+
+//  let ex1='50';
+// // ex1=parseInt(ex1)
+// // console.log(typeof(ex1));
+// ex1.toString();
+
+
+// let money=500;
+// let budget=2000;
+
+// if(budget<=money){
+//     console.log("You can Buy ");
+//     console.log('Price of gift : 200')
+//     console.log("Your balance", money-budget);
+// }
+// else{
+//     console.log("You don't have enough Money");
+//     console.log("You have:",money);
+//     console.log("Cost:",budget);
+// }
+
+// let nam="fgdfgd";
+
+// if(nam.endsWith('d')){
+//     console.log(nam.toUpperCase());
+// }
+
+
+// let count=10;
+
+// while(count<=100){
+//     console.log("Ravi");
+//     count++;
+// }
+
+
+//objects
+
+// let car = { 
+//             Type:"sport",
+//             model:"koiensegg",
+//             color:"Blue"
+// }
+// console.log(typeof(car));
+// car.type="toyata"
+// car.wheels=4;
+// console.log(car.type,car.wheels,car.color);
+
+
+//functions
+
+// function Greet(){
+//     console.log("Hello bro");
+// }
+// Greet();
+
+// function calc (a ,  b){
+//     if(a>b){
+//         console.log(a , "Greater than ",b);
+//     }
+//     else{
+//         console.log(b , "Greater than ",a);
+//     }
+// }
+
+// calc(20,40);
+
+
+function myFunction(a,b){
+    return a*b;
+    console.log(a*b);
+
+}
+
+myFunction(3,5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
